fbf7dc3971976969262f94e45378d02176b50987
Andrey <x137@yandex.ru>
2021-09-16 00:57:57 +0300

    update annotation ValidEmail in 11 string =) correct import

--- src/main/java/project/annotations/ValidEmail.java
+++ src/main/java/project/annotations/ValidEmail.java
@@ -1,6 +1,6 @@
 package project.annotations;
 
-import org.hibernate.validator.internal.constraintvalidators.bv.EmailValidator;
+import project.validations.EmailValidator;
 
 import javax.validation.Constraint;
 import javax.validation.Payload;
