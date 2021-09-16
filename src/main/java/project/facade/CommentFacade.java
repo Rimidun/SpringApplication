9725cae8a2c4ce033e460cbf0f4eb54f795cf81c
Andrey <x137@yandex.ru>
2021-09-16 23:52:35 +0300

    package facade and comment, post, user facade

---
+++ src/main/java/project/facade/CommentFacade.java
@@ -0,0 +1,21 @@
+package project.facade;
+
+import org.springframework.stereotype.Component;
+import project.dto.CommentDTO;
+import project.entity.Comment;
+
+@Component
+public class CommentFacade {
+    public CommentDTO commentDTO(Comment comment) {
+
+
+        CommentDTO commentDTO = new CommentDTO();
+        commentDTO.setId(comment.getId());
+        commentDTO.setUsername(comment.getUsername());
+        commentDTO.setMessage(comment.getMessage());
+
+        return commentDTO;
+
+    }
+
+}
---
+++ src/main/java/project/facade/PostFacade.java
@@ -0,0 +1,24 @@
+package project.facade;
+
+import org.springframework.stereotype.Component;
+import project.dto.PostDTO;
+import project.entity.Post;
+
+@Component
+public class PostFacade {
+
+    public PostDTO postToPostDTO(Post post) {
+        PostDTO postDTO = new PostDTO();
+        postDTO.setUsername(post.getUser().getUsername());
+        postDTO.setId(post.getId());
+        postDTO.setCaption(post.getCaption());
+        postDTO.setLikes(post.getLikes());
+        postDTO.setUsersLiked(post.getLikedUsers());
+        postDTO.setLocation(post.getLocation());
+        postDTO.setTitle(postDTO.getTitle());
+
+        return postDTO;
+
+    }
+
+}
